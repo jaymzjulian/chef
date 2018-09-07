e396afe9dc9ffad19fc799944f7c06e6276a5cae
Tim Smith <tsmith@chef.io>
2018-09-06 17:54:56 -0700

    Generate a nicely formatted example resource block
    Yank logic out of erb so we can add all the padding logic we need to align things.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- docs.rb
+++ docs.rb
@@ -7,6 +7,35 @@ $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "lib")))
 require "chef/resource_inspector"
 require "erb"
 
+# generate the top example resource block example text
+# @return String
+def generate_resource_block(resource_name, properties)
+  padding_size = largest_property_name(properties) + 6
+
+  # build the resource string with property spacing between property names and comments
+  text = "  #{resource_name} 'name' do\n"
+  properties.each do |p|
+    next if p["name"] == "name"
+    text << "    #{p['name'].ljust(padding_size)}"
+    text << friendly_types_list(p["is"])
+    text << " # default value: #{p['default']}" unless p["default"].nil? || p["default"] == "lazy default"
+    text << "\n"
+  end
+  text << "    #{'action'.ljust(padding_size)}# defaults to :#{@default_action.first} if not specified\n"
+  text << "  end"
+  text
+end
+
+# we need to know how much space to leave so columns line up
+# @return String
+def largest_property_name(properties)
+  if properties.empty?
+    0
+  else
+    properties.max_by { |x| x["name"].size }["name"].size
+  end
+end
+
 # given an array ``bold`` the values, comma separate them, and put include 'and' if 3+
 # @return String
 def bolded_friendly_list(arr)
@@ -14,7 +43,7 @@ def bolded_friendly_list(arr)
   if arr.size > 2
     arr[-1] = "and #{arr[-1]}"
   end
-  arr.join(', ')
+  arr.join(", ")
 end
 
 # given an array of types print out a single comma separated string
@@ -36,7 +65,7 @@ def friendly_types_list(arr)
       end
     end
   end
-  fixed_arr.join(', ')
+  fixed_arr.join(", ")
 end
 
 # Makes sure the resource name is bolded within the description
@@ -47,7 +76,7 @@ def bolded_description(name, description)
 end
 
 template = %{=====================================================
-<%= @name %>
+<%= @name %> resource
 =====================================================
 `[edit on GitHub] <https://github.com/chef/chef-web-docs/blob/master/chef_master/source/resource_<%= @name %>.rst>`__
 
@@ -57,17 +86,11 @@ template = %{=====================================================
 <% end %>
 Syntax
 =====================================================
-This resource has the following syntax:
+The <%= @name %> resource has the following syntax:
 
 .. code-block:: ruby
 
-   <%= @name %> 'name' do
-<% @properties.each do |p| %>
-<% next if p['name'] == 'name' %>
-     <%= p['name'] %>                     <%= friendly_types_list(p['is']) %><% unless p['default'].nil? || p['default'] == "lazy default"  %> # default value: <%= p['default'] %><% end %>
-<% end %>
-     action                     Symbol # defaults to :<%= @default_action.first %> if not specified
-   end
+<%= @resource_block %>
 
 where:
 
@@ -77,7 +100,7 @@ where:
 Actions
 =====================================================
 
-This resource has the following actions:
+The <%= @name %> resource has the following actions:
 <% @actions.each do |a| %>
 ``:<%= a %>``
    <% if a == @default_action %>Default. <% end %> Description here.
@@ -92,7 +115,7 @@ This resource has the following actions:
 Properties
 =====================================================
 
-This resource has the following properties:
+The <%= @name %> resource has the following properties:
 <% @properties.each do |p| %>
 ``<%= p['name'] %>``
    **Ruby Type:** <%= friendly_types_list(p['is']) %><% unless p['default'].nil? %> | **Default Value:** ``<%= p['default'] %>``<% end %><% if p['deprecated'] %> | ``DEPRECATED``<% end %><% if p['name_property'] %> | **Default Value:** ``'name'``<% end %>
@@ -104,19 +127,21 @@ This resource has the following properties:
 
 resources = Chef::JSONCompat.parse(ResourceInspector.inspect)
 resources.each do |resource, data|
-  next if %w(l_w_r_p_base user_resource_abstract_base_class linux_user pw_user aix_user dscl_user solaris_user windows_user).include?(resource) || resource.nil?
+  next if ["l_w_r_p_base", "user_resource_abstract_base_class", "linux_user", "pw_user", "aix_user", "dscl_user", "solaris_user", "windows_user", ""].include?(resource)
   puts "Writing out #{resource}."
   @name = resource
-  @description = data['description']
-  @default_action = data['default_action']
-  @actions = (data['actions'] - ["nothing"]).sort
-  @examples = data['examples']
-  @introduced = data['introduced']
-  @preview = data['preview']
-  @properties = data['properties'].sort_by! { |v| v['name'] }
+  @description = data["description"]
+  @default_action = data["default_action"]
+  @actions = (data["actions"] - ["nothing"]).sort
+  @examples = data["examples"]
+  @introduced = data["introduced"]
+  @preview = data["preview"]
+  @properties = data["properties"].sort_by! { |v| v["name"] }
+  @resource_block = generate_resource_block(resource, @properties)
+  #require 'pry'; binding.pry
 
   t = ERB.new(template)
-  File.open("resource_#{@name}.rst", 'w') do |f|
-  f.write t.result(binding)
+  File.open("resource_#{@name}.rst", "w") do |f|
+    f.write t.result(binding)
   end
 end
